a677485e921e33ea21cff33c43b7ef23a0da8672
konstantinoscs <kons.ste@gmail.com>
2017-10-09 01:27:41 +0300

    fixed the format

--- Part_1/data_ops.h
+++ Part_1/data_ops.h
@@ -1,4 +1,6 @@
 #ifndef DATA_OPS_H
 #define DATA_OPS_H
 
+int parse_arguments(int argc, char ** argv, std::string & data_s,
+  std::string & query_s );
 #endif
--- Part_1/main.cpp
+++ Part_1/main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 #include "data_ops.h"
 
 using namespace std;
 
 int main(int argc, char **argv){
+  //k = locality sensitive functions
+  //L  = number of hasthables
+  int k = 4, L = 5;
+  string data_s, query_s;
+
   ifstream data("");
-  ifstream data("")
+  ifstream query("");
+
 }
