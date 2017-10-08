3ff16c34202b2503a2e194f2976e13ec6efe8f68
konstantinoscs <kons.ste@gmail.com>
2017-10-08 18:14:52 +0300

    created files for data operations and updated gitignore

---
+++ Part_1/data_ops.h
@@ -0,0 +1,4 @@
+#ifndef DATA_OPS_H
+#define DATA_OPS_H
+
+#endif
--- Part_1/main.cpp
+++ Part_1/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <fstream>
+
+#include "data_ops.h"
 
 using namespace std;
 
 int main(int argc, char **argv){
-
+  ifstream data("");
+  ifstream data("")
 }
