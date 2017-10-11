d0c9fe872bd58848b1c1a1a513b19b56f5b40519
konstantinoscs <kons.ste@gmail.com>
2017-10-11 23:57:22 +0300

    avoid too many variable declarations

--- Part_1/curve_ops.cpp
+++ Part_1/curve_ops.cpp
@@ -9,13 +9,16 @@ curve curve_reduction(curve c, float delta){
   curve cur = new curve(c.get_id(),c.get_dimension());
   vector<vector<float>> points = c.get_points();
   vector<vector<float>> new_points{};
-  float element;
-  float p = delta/2;
+  float element{};
+  float p{delta/2};
+  int negative_bit{};
+  float current_p{};
+
   for(int i=0; i<points.size(); i++){
     vector<float> point{};
     for(int j=0; j<points[i].size(); j++){
-      int negative_bit{0};
-      float current_p = points[i][j];
+      negative_bit = 0;
+      current_p = points[i][j];
       if(current_p != abs(points[i][j]))
         negative_bit = 1;
       //element = (x + d/2)/d
