5db7551eb74cac5458e7450ea9610db74d6fe637
konstantinoscs <kons.ste@gmail.com>
2017-10-12 02:52:03 +0300

    put in comments to compile

--- Part_1/curve_ops.cpp
+++ Part_1/curve_ops.cpp
@@ -6,27 +6,28 @@
 using namespace std;
 
 curve curve_reduction(const curve & ur_curve, float delta){
-  curve cur = new curve(ur_curve.get_id(), ur_curve.get_dimension());
-  vector<vector<float>> points = ur_curve.get_points();
+  //ur_curve --> unreducted curve
+  //curve cur = new curve(ur_curve.get_id(), ur_curve.get_dimension());
+  //vector<vector<float>> points = ur_curve.get_points();
   vector<vector<float>> new_points;
   float element{};
   float p{delta/2};
   int negative_bit{};
   float current_p{};
 
-  for(int i=0; i<points.size(); i++){
-    vector<float> point{};
-    for(int j=0; j<points[i].size(); j++){
-      negative_bit = 0;
-      current_p = points[i][j];
-      if(current_p != abs(points[i][j]))
-        negative_bit = 1;
-      //element = (x + d/2)/d
-      element = (points[i][j] + p)/delta;
-      point.push_back(element*delta*negative_bit);
-    }
-    new_points.push_back(point);
-  }
-  cur.set_points(new_points);
-  return cur;
+  // for(int i=0; i<points.size(); i++){
+  //   vector<float> point{};
+  //   for(int j=0; j<points[i].size(); j++){
+  //     negative_bit = 0;
+  //     current_p = points[i][j];
+  //     if(current_p != abs(points[i][j]))
+  //       negative_bit = 1;
+  //     //element = (x + d/2)/d
+  //     element = (points[i][j] + p)/delta;
+  //     point.push_back(element*delta*negative_bit);
+  //   }
+  //   new_points.push_back(point);
+  // }
+  // ur_curve.set_points(new_points);
+  // return cur;
 }
