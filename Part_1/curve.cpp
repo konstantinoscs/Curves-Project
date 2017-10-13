3198107c9965abf7c19dbea9252b772d0e396fe3
konstantinoscs <kons.ste@gmail.com>
2017-10-13 03:14:47 +0300

    Completed inherritance for real and canonical curves

--- Part_1/curve.cpp
+++ Part_1/curve.cpp
@@ -1,10 +1,11 @@
-#include "curve.h"
+#include <iostream>
 #include <vector>
+#include "curve.h"
 
 using namespace std;
 
 curve::curve(int dim): dimension(dim){
-
+  cout << "Default constructor for curve called" << endl;
 }
 
 curve::~curve(){
@@ -21,4 +22,44 @@ string curve::get_id(){
 
 int curve::get_dimension(){
   return dimension;
+}
+
+real_curve::real_curve(){
+
+}
+
+real_curve::~real_curve(){
+
+}
+
+void real_curve::set_point(std::vector<double> & point){
+  points.push_back(point);
+}
+
+void real_curve::set_point(std::vector<double> && point){
+  points.push_back(point);
+}
+
+const std::vector<std::vector<double>> & real_curve::get_points(){
+  return points;
+}
+
+can_curve::can_curve(){
+
+}
+
+can_curve::~can_curve(){
+
+}
+
+void can_curve::set_point(std::vector<int> & point){
+  points.push_back(point);
+}
+
+void can_curve::set_point(std::vector<int> && point){
+  points.push_back(point);
+}
+
+const std::vector<std::vector<int>> & can_curve::get_points(){
+  return points;
 }
--- Part_1/curve.h
+++ Part_1/curve.h
@@ -7,15 +7,40 @@
 class curve{
   std::string id;
   int dimension;
-  //every point is a dimensiom-vector of coordinates
-  std::vector<std::vector<double>> points;
+  //every point is a dimension-vector of coordinates
+  // std::vector<std::vector<double>> points;
 public:
   curve(int dim = 0);
-  ~curve();
+  virtual ~curve();
   void set_id(std::string r_id);
   std::string get_id();
   int get_dimension();
 
+};
+
+//real_curve and can_curve are symmetrical with double and int coordinates
+class real_curve : public curve {
+  std::vector<std::vector<double>> points;
+
+public:
+  real_curve();
+  ~real_curve();
+  void set_point(std::vector<double> & point);
+  //we introduce a move push back
+  void set_point(std::vector<double> && point);
+  const std::vector<std::vector<double>> & get_points();
+};
+
+class can_curve : public curve {
+  std::vector<std::vector<int>> points;
+
+public:
+  can_curve();
+  ~can_curve();
+  void set_point(std::vector<int> & point);
+  //we introduce a move push back
+  void set_point(std::vector<int> && point);
+  const std::vector<std::vector<int>> & get_points();
 };
 
 #endif
