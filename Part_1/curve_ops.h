856860b6eb4985a823566ece7836100583a12dc6
konstantinoscs <kons.ste@gmail.com>
2017-10-14 22:02:03 +0300

    80 char /line limit

--- Part_1/curve_ops.h
+++ Part_1/curve_ops.h
@@ -7,6 +7,8 @@ curve curve_reduction(const curve & ur_curve, double delta);
 
 void chosen_t(double delta, int dimension, std::vector<double> & t);
 
-void curve_move(const std::vector<std::vector<double>> & norm_points,const std::vector<double> & t, const int & dimension, const int & max, std::vector<std::vector<double>> & moved_points);
+void curve_move(const std::vector<std::vector<double>> & norm_points,
+  const std::vector<double> & t, const int & dimension, const int & max,
+  std::vector<std::vector<double>> & moved_points);
 
 #endif
