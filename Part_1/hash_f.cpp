383e3b17db676472f0d83cd46e2720ff63a28963
konstantinoscs <kons.ste@gmail.com>
2017-10-16 03:46:27 +0300

    general fixes

--- Part_1/hash_f.cpp
+++ Part_1/hash_f.cpp
@@ -1,4 +1,4 @@
-
+#include <cmath>
 #include "hash_f.h"
 
 hash_f::hash_f(int t_id, int t_t) : t(t_t), id(t_id){
@@ -10,13 +10,13 @@ hash_f::~hash_f(){
 }
 
 int hash_f::hash(const std::vector<int> & p, int w){
-  int sum{} //maybe long double??
-  for(int i=0; i<.size(); i++){
+  int sum{}; //maybe long double??
+  for(int i=0; i<v.size(); i++){
     sum += v[i]*p[i];
   }
   return floor((float)(sum+t) / w);
 }
 
-hash_f::populate_v(){
-  
+void hash_f::populate_v(){
+
 }
--- Part_1/rand_utils.cpp
+++ Part_1/rand_utils.cpp
@@ -1,8 +1,9 @@
 #include <cstdlib>
+#include <cmath>
 
 //uniform random in [A,B]
 double get_rand_uniform(double A, double B){
-  return B + (rand() / RAND_MAX + 1)
+  return B + (rand() / (RAND_MAX + 1.0)) * (B - A + 1);
 }
 
 double get_rand_uniform(double B){
