8296ee2925ba5cd6e85090ca82a6b754516d95a6
konstantinoscs <kons.ste@gmail.com>
2017-10-17 04:01:53 +0300

    copy and move constructor

--- Part_1/hash_f.cpp
+++ Part_1/hash_f.cpp
@@ -1,10 +1,13 @@
 #include <cmath>
+#include <iostream>
 #include "hash_f.h"
 #include "rand_utils.h"
 
+using namespace std;
+
 //chose t at construction
 hash_f::hash_f(int t_id, int t_w, int v_size) : id(t_id), w(t_w){
-  t = get_uniform_rand(w);
+  t = double_uniform_rand(w);
   populate_v(v_size);
 }
 
@@ -13,6 +16,16 @@ hash_f::hash_f(int t_id, float t_t, int t_w, int v_size) : id(t_id), t(t_t),
 w(t_w){
   //initialize the v
   populate_v(v_size);
+}
+
+hash_f::hash_f(const hash_f & c_hash): id(c_hash.id), t(c_hash.t), w(c_hash.w),
+  v(c_hash.v){
+
+}
+
+hash_f::hash_f(hash_f && m_hash): id(m_hash.id), t(m_hash.t), w(m_hash.w),
+  v(std::move(m_hash.v)){
+  m_hash.v.clear();
 }
 
 hash_f::~hash_f(){
@@ -25,6 +38,15 @@ int hash_f::hash(const std::vector<int> & p){
   }
   //(float) is necessary here
   return floor((float)(sum+t) / w);
+}
+
+void hash_f::print(){
+  cout << "hash_f i with id " << id << endl;
+  cout << "t " << t << endl;
+  cout << "w " << w << endl;
+  cout << "v :" << endl;
+  for (int i=0; i<v.size(); i++)
+    cout << v[i] << endl;;
 }
 
 void hash_f::populate_v(int v_size){
--- Part_1/hash_f.h
+++ Part_1/hash_f.h
@@ -15,8 +15,11 @@ public:
   //you can give a t or t is chosen at construction
   hash_f(int t_id, int t_w, int v_size);
   hash_f(int t_id, float t_t, int t_w, int v_size);
+  hash_f(const hash_f & c_hash);
+  hash_f(hash_f && m_hash);
   ~hash_f();
   int hash(const std::vector<int> & p);
+  void print();
 
 };
 #endif
