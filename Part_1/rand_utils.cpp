56236786b585dad5f3ea3a17515e38eff98301bd
konstantinoscs <kons.ste@gmail.com>
2017-10-17 04:01:43 +0300

    clearer names in rand_utils

--- Part_1/rand_utils.cpp
+++ Part_1/rand_utils.cpp
@@ -2,21 +2,25 @@
 #include <cmath>
 
 //uniform random in [A,B]
-double get_uniform_rand(double A, double B){
+double double_uniform_rand(double A, double B){
   //+1.0 implicitly converts to float division
   return A + (rand() / (RAND_MAX + 1.0)) * (B - A);
 }
 
-double get_uniform_rand(double B){
-  return get_uniform_rand(0, B);
+int int_uniform_rand(int A, int B){
+  return A + (rand() / (RAND_MAX + 1.0)) * (B - A);
+}
+
+double double_uniform_rand(double B){
+  return double_uniform_rand(0.0, B);
 }
 
 //normal random in [A,B]
 double marsaglia_normal_rand(){
   double u{}, v{}, x{}, s{2};
   while (s>=1){
-    u = get_uniform_rand(-1, 1);
-    v = get_uniform_rand(-1, 1);
+    u = double_uniform_rand(-1.0, 1.0);
+    v = double_uniform_rand(-1.0, 1.0);
     s = u*u + v*v;
   }
   x = u * sqrt(-2*log(s)/s);
--- Part_1/rand_utils.h
+++ Part_1/rand_utils.h
@@ -2,10 +2,13 @@
 #define RAND_UTILS_H
 
 //uniform random in [A,B]
-double get_uniform_rand(double A, double B);
+double double_uniform_rand(double A, double B);
+
+//int uniform random in [A,B]
+int int_uniform_rand(int A, int B);
 
 //uniform random in [0,B]
-double get_uniform_rand(double B);
+double double_uniform_rand(double B);
 
 //standard normal random
 double marsaglia_normal_rand();
