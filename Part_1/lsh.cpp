c72a42a94bef52e248588283f58e475b495b2526
konstantinoscs <kons.ste@gmail.com>
2017-10-22 04:49:00 +0300

    documentation of lsh

--- Part_1/lsh.cpp
+++ Part_1/lsh.cpp
@@ -7,16 +7,21 @@
 
 using namespace std;
 
+//makes k hash functions and puts them in hs
 void make_hashes(vector<hash_f> & hs, int w, int v_size, int k){
   for(int i =0; i<k; i++)
     hs.push_back(std::move(hash_f(i, w, v_size)));
 }
 
-void make_g(const vector<hash_f> hs, int * g){
+//choose k h_i randomly, the indexes of the hash functions are
+//put in an int array g
+void make_g(const vector<hash_f> & hs, int * g){
   for(int i=0; i<hs.size(); i++)
     g[i] = int_uniform_rand(0, hs.size()-1);
 }
 
+//the lsh hashing, most probably will be omitted and integrated in
+//hash_curves of "classic_hash.cpp"
 void lsh_hash_curves(int k, int w, int v_size, int dimension, int tablesize,
   const vector<vector<norm_curve>> & Lcurves){
 
@@ -26,5 +31,4 @@ void lsh_hash_curves(int k, int w, int v_size, int dimension, int tablesize,
 
   make_hashes(hs, w, v_size, k);
   init_r(dimension, r);
-
 }
--- Part_1/lsh.h
+++ Part_1/lsh.h
@@ -4,10 +4,15 @@
 #include <vector>
 #include "hash_f.h"
 
+//makes k hash functions and puts them in hs
 void make_hashes(std::vector<hash_f> & hs, int w, int v_size, int k);
 
-void make_g(const std::vector<hash_f> hs, int * g);
+//choose k h_i randomly, the indexes of the hash functions are
+//put in an int array g
+void make_g(const std::vector<hash_f> & hs, int * g);
 
+//the lsh hashing, most probably will be omitted and integrated in
+//hash_curves of "classic_hash.cpp"
 void lsh_hash_curves(int k, int w, int v_size, int dimension, int tablesize,
   const std::vector<std::vector<norm_curve>> & Lcurves);
 
