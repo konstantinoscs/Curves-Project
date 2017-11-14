f179b6820720e2ad1bf60119b2128ec54dd295b7
Unknown <stefanospantoulas@gmail.com>
2017-11-15 01:47:49 +0200

    inits done

--- Part_2/initialization.cpp
+++ Part_2/initialization.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <string>
 
 #include"rand_utils.h"
 #include "curve.h"
@@ -14,43 +15,60 @@ void random_init(vector<real_curve*> & curves,int k,
   for(int i=0; i<k; i++){
     index = int_uniform_rand(0,curves.size()-1);
     centroid.push_back(curves[index]);
-    curves.erase(curves.begin() + index);
+    curves.erase(curves.begin() + index);//remove centroid
   }
   return ;
 }
 
+int find_t(vector<double> A,double x,int start,int end){
+	int m =start + (end - start)/2;
+	if(x==A[start])
+		return start;
+	else if(x==A[end])
+		return end-1;
+	else if((start==end) || (end==start+1))
+		return end-1;
+	else if(A[m] < x)
+		return find_t(A,x,m,end);
+	else
+		return find_t(A,x,start,m);
+}
+
 void kmeans_init(vector<real_curve*> & curves,int k,
-      vector<real_curve*> & centroid){
+      vector<real_curve*> & centroid, string dist){
   centroid.clear();
   int index{int_uniform_rand(0,curves.size()-1)};
   centroid.push_back(curves[index]);//first centroid
-  curves.erase(curves.begin() + index);
-  for(int z=0; z<k-1; z++){//finds the k-1 centroids
+  curves.erase(curves.begin() + index);//remove 1st centroid
+  for(int z=0; z<k-1; z++){//lets find the k-1 remaining centroids
     vector<double> Sum{};
     double temp_min1{},temp_min2{}, new_t{};
     int t{};//centroid's index
     //first rep before loop to find Sum[0];
-    find_distance(centroid[0]->get_points(), curves[0]->get_points(), "DFT",temp_min1);
+    find_distance(centroid[0]->get_points(), curves[0]->get_points(),
+      dist, temp_min1);
     for(unsigned int j=1; j<centroid.size(); j++){
-      find_distance(centroid[j]->get_points(), curves[0]->get_points(), "DFT",temp_min2);
-      temp_min1 = MYmin(temp_min1,temp_min2);
+      find_distance(centroid[j]->get_points(), curves[0]->get_points(),
+        dist, temp_min2);
+      temp_min1 = MYmin(temp_min1,temp_min2);//keeps the min dist between c
     }
     Sum.push_back(temp_min1*temp_min1);//Sum[0] = d[0]^2
     //rest curves.size()-1 reps...
     for(unsigned int i=0; i<curves.size(); i++){
-      find_distance(centroid[0]->get_points(), curves[i]->get_points(), "DFT",temp_min1);
+      find_distance(centroid[0]->get_points(), curves[i]->get_points(),
+        dist, temp_min1);
       for(unsigned int j=1; j<centroid.size(); j++){
-        find_distance(centroid[j]->get_points(), curves[0]->get_points(), "DFT",temp_min2);
+        find_distance(centroid[j]->get_points(), curves[0]->get_points(),
+          dist, temp_min2);
         temp_min1 = MYmin(temp_min1,temp_min2);
       }
-      Sum.push_back(Sum[i-1] + temp_min1*temp_min1);//Sum[i] = Sum[i-1] +d[i]^2
+      Sum.push_back(Sum[i-1] + temp_min1*temp_min1);//Sum[i] = Sum[i-1]+d[i]^2
     }
     new_t = double_uniform_rand(Sum[Sum.size()-1]);
-    //t = find_t(Sum, new_t, 0.0, Sum.size()-1);
-    centroid.push_back(curves[t]);
-    curves.erase(curves.begin() + t);
+    t = find_t(Sum, new_t, 0, Sum.size()-1);//finds new centroid's index
+    centroid.push_back(curves[t]);//push it here
+    curves.erase(curves.begin() + t);//and remove it from here
   }
   return ;
 }
-
 
--- Part_2/initialization.h
+++ Part_2/initialization.h
@@ -1,12 +1,22 @@
 #ifndef INITIALIZATION_H
 #define INITIALIZATION_H
 
-#include<vector>
+#include <vector>
+#include <string>
 
 #include "curve.h"
 
-void random_init(std::vector<real_curve*> & curves,int k,std::vector<real_curve*> & centroid);
+void random_init(
+  std::vector<real_curve*> & curves,//start:has all curves,end:without the k centroids
+  int k,//number of centroids
+  std::vector<real_curve*> & centroid);//in the end has the k centroids
 
-void kmeans_init(std::vector<real_curve*> & curves,int k,std::vector<real_curve*> & centroid);
+//same with above function
+void kmeans_init(std::vector<real_curve*> & curves,int k,
+       std::vector<real_curve*> & centroid, std::string dist);
+
+//A is a sorted array
+//returns the A's index which has the min value bigger than x
+int find_t(vector<double> A,double x,int start,int end);
 
 #endif
