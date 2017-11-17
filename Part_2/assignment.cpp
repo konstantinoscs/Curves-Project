edc71dc6e7296434d6126f4826bd9b11b3ea57b2
konstantinoscs <kons.ste@gmail.com>
2017-11-17 21:06:21 +0200

    quick fix

--- Part_2/assignment.cpp
+++ Part_2/assignment.cpp
@@ -11,12 +11,12 @@ bool lloyds_assignment(vector<real_curve *> centroids,
   double distmin{}, dist{};
 
   if(!assignment.empty())
-    assignment.resize(centroids.size());  
+    assignment.resize(centroids.size());
 
   for(int i=0; i<curves.size(); i++){
     jmin = 0;
     const vector<vector<double>> & c_points = curves[i]->get_points();
-    computeDFD(c_points, centroids[0]->get_points(), dist);
+    computeDFD(c_points, centroids[0]->get_points(), distmin);
     for (int j=1; j<centroids.size(); j++){
       computeDFD(c_points, centroids[j]->get_points(), dist);
       if(dist < distmin){
