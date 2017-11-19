c216e625136abfc9ebf5049ccaac80b050b347d4
konstantinoscs <kons.ste@gmail.com>
2017-11-19 23:50:02 +0200

    update and comments

--- Part_2/assignment.cpp
+++ Part_2/assignment.cpp
@@ -10,13 +10,16 @@ bool lloyds_assignment(vector<real_curve *> centroids,
   int jmin{};
   double distmin{}, dist{};
 
+  //we need to access arbitary indexes so ensure that they exist
   if(!assignment.empty())
     assignment.resize(centroids.size());
 
   for(int i=0; i<curves.size(); i++){
     jmin = 0;
     const vector<vector<double>> & c_points = curves[i]->get_points();
+    //set as min distance the distance from the first centroid
     computeDFD(c_points, centroids[0]->get_points(), distmin);
+    //compare the min with all the other distances
     for (int j=1; j<centroids.size(); j++){
       computeDFD(c_points, centroids[j]->get_points(), dist);
       if(dist < distmin){
