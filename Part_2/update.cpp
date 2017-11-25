42a03410597b99ef615406e3627f2df055156ee0
konstantinoscs <kons.ste@gmail.com>
2017-11-25 17:24:52 +0200

    should be working, needs to be tested

--- Part_2/update.cpp
+++ Part_2/update.cpp
@@ -3,24 +3,67 @@
 #include <string>
 #include "curve.h"
 #include "distance_ops.h"
+#include "mean_curves.h"
 
 using namespace std;
 
 double compute_objective(vector<real_curve *> & centroids,
   vector<vector<real_curve *>> & assignment, string func);
 
-double mean_discrete_frechet(std::vector<real_curve *> & centroids,
-  std::vector<std::vector<real_curve *>> & assignment, double objective){
+real_curve * cluster_centroid(vector<real_curve *> & cluster){
+  vector<real_curve *> means;
+  //compute centroids from the dataset curves (bottom leaf level)
+  for(size_t i=0; i<cluster.size(); i+=2){
+    if(i+1<cluster.size())
+      means.push_back(find_mean(cluster[i], cluster[i+1]));
+    else
+      means.push_back(cluster[i]);
+  }
 
+  //start the reduction until you get to the root
+  while(means.size()>1){
+    vector<real_curve *> temp_means;
+    for(size_t i=0; i<means.size(); i+=2){
+
+      if(i+1<cluster.size()){
+        temp_means.push_back(find_mean(means[i], means[i+1]));
+
+        //check if the curves were temporary means and delete them
+        //(in the case of odd number of curves one leaf will be part
+        // of the original dataset so don't delete it)
+        if(!means[i]->get_id().compare("-1"))
+          delete means[i];
+        if(means[i+1]->get_id().compare("-1"))
+          delete means[i+1];
+      }
+      else{
+        temp_means.push_back(means[i]);
+      }
+    }
+    means = std::move(temp_means);
+  }
+
+  return means.back();
+}
+
+bool mean_discrete_frechet(vector<real_curve *> & centroids,
+  vector<vector<real_curve *>> & assignment){
+
+  vector<real_curve *> temp_centroids;
   //find which centroids were mean curves (not in dataset) and
   //delete them
   for(size_t i=0; i<centroids.size(); i++){
-    if(centroid[i]->get_id() == -1)
-      delete centroid[i];
-    centroid[i] = nullptr;
+    if(centroids[i]->get_id().compare("-1"))
+      delete centroids[i];
+    centroids[i] = nullptr;
   }
 
+  //for each cluster compute the new centroid
+  for(size_t i=0; i<centroids.size(); i++){
+    centroids[i] = cluster_centroid(assignment[i]);
+  }
 
+  return true;
 }
 
 double pam_update(vector<real_curve *> & centroids,
--- Part_2/update.h
+++ Part_2/update.h
@@ -3,10 +3,9 @@
 
 #include "curve.h"
 
-//mean frechet chooses a centroid which is not part of the
-//dataset
-double mean_discrete_frechet(std::vector<real_curve *> & centroids,
-  std::vector<std::vector<real_curve *>> & assignment, double objective);
+//mean frechet chooses a centroid which is not part of the dataset
+bool mean_discrete_frechet(std::vector<real_curve *> & centroids,
+  std::vector<std::vector<real_curve *>> & assignment);
 
 //pam chooses a medoid
 double pam_update(std::vector<real_curve *> & centroids,
