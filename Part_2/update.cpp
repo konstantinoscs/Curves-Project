48da00ff156d9417409bd4cef8569c0a290149e3
konstantinoscs <kons.ste@gmail.com>
2017-11-26 18:33:45 +0200

    gj

--- Part_2/update.cpp
+++ Part_2/update.cpp
@@ -100,22 +100,13 @@ double compute_objective(vector<real_curve *> & centroids,
   vector<vector<real_curve *>> & assignment, string func){
 
   double objective{}, dist{};
-  //distance is a pointer to the distance function we'll use
-  void (*distance)(const vector<vector<double>> & pointsA,
-    const vector<vector<double>> & pointsB, double & distance);
-
-  //initialize pointer
-  if(!func.compare("DFT"))
-    distance = &computeDFD;
-  else
-    distance = &computeDTW;
 
   for(size_t i=0; i<centroids.size(); i++){
     //get centroid points
     const vector<vector<double>> & c_points = centroids[i]->get_points();
     //for every curve in cluster add distance to centroid
     for(size_t j=0; j<assignment[i].size(); j++)
-    (*distance)(c_points, assignment[i][j]->get_points(), dist);
+    find_distance(c_points, assignment[i][j]->get_points(), func, dist);
     objective += dist;
   }
   return objective;
