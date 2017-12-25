29905808ba5eb2388c71deaf035e09fbeef7d584
konstantinoscs <kons.ste@gmail.com>
2017-12-25 22:34:59 +0200

    compiles

--- Part_3/Roads/xml_ops.cpp
+++ Part_3/Roads/xml_ops.cpp
@@ -1,6 +1,9 @@
+#include <fstream>
 #include <iostream>
-#include <string>
 #include <limits>
+#include <string>
+#include <utility>
+#include <vector>
 
 using namespace std;
 
@@ -8,17 +11,43 @@ struct node {
   string id;
   double lat;
   double lon;
+  node();
+  //move constructor
+  node(node && mnode);
+  //copy constructor in case move fails
+  node(node & cnode);
 };
+
+node::node():id(""),lat(0), lon(0){}
+
+node::node(node && mnode):id(std::move(mnode.id)), lat(mnode.lat),
+  lon(mnode.lon){
+  mnode.id.clear();
+}
+
+node::node(node & cnode):id(cnode.id), lat(cnode.lat), lon(cnode.lon){}
 
 bool parse_xml(string data_s){
   ifstream data(data_s);
   vector<node> nodes;
+  node t_node;
   string temp{};
-  max = std::numeric_limits<int>;
+  size_t max = std::numeric_limits<streamsize>::max();
   //ignore the first 3 lines of the xml file
   data.ignore(max, '\n');
   data.ignore(max, '\n');
   data.ignore(max, '\n');
+  //start reading nodes;
+  data >> temp;
+  while(true){
+    //if it's not a node tag break
+    if(!temp.compare("<node")){
+      data >> temp;
+      //copy the id
+      t_node.id = std::move(temp.substr(4, temp.length()-2));
+      data >> temp;
+      t_node.lat = stod(temp.substr(5));
+  }
 
-
+}
 }
